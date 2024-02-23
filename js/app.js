83d5bc9aabff4898e6d86cc10cb6db089754c7d9
BenjaminLG <bleguellec.dev@gmail.com>
2024-02-23 19:22:45 +0100

    Script pour la réalisation de la grille

--- js/app.js
+++ js/app.js
@@ -3,17 +3,16 @@ console.log('DeViosaure 4ever !!')
 
 // CREATION DU FORMULAIRE
 
-
-
 const createForm = () => {
 
     const headerElement = document.querySelector('header');
-    const formElement = document.createElement('form');
+
+    const FormElement = document.createElement('form');
     const inputElementGrid = document.createElement('input');
     const inputElementPixel = document.createElement('input');
     const buttonElementForm = document.createElement('button');
 
-    formElement.className = ('formulaire');
+    FormElement.className = ('formulaire');
     inputElementGrid.className = ('formulaire__grille');
     inputElementPixel.className = ('formulaire__pixel');
     buttonElementForm.className = ('formulaire__bouton');
@@ -22,8 +21,48 @@ const createForm = () => {
     inputElementPixel.placeholder = 'Taille des pixel';
     buttonElementForm.textContent = 'Valider';
 
-    headerElement.append(formElement);
-    formElement.append(inputElementGrid, inputElementPixel, buttonElementForm);
+    headerElement.append(FormElement);
+    FormElement.append(inputElementGrid, inputElementPixel, buttonElementForm);
 }
 
 createForm();
+
+
+// CREATION DE LA GRILLE
+
+const infoGrid = {
+    column : 8,
+    pixel : 8,
+    pixelSize : "",
+}
+
+
+
+const mainElement = document.querySelector('main');
+
+for (let i = 0; i < infoGrid.column; i++) {
+    const sectionElementGrid = document.createElement('section');
+    sectionElementGrid.className = ('grille');
+    mainElement.append(sectionElementGrid);
+
+    for (let i = 0; i < infoGrid.pixel; i++) {
+        const divElementPixel = document.createElement('div');
+        divElementPixel.className = ('grille__pixels');
+        sectionElementGrid.append(divElementPixel);
+    }
+}
+
+
+const pixelElement = document.querySelector('.grille__pixels');
+
+pixelElement.addEventListener('click', () => {
+    pixelElement.classList.toggle('red-pixel');
+})
+
+// VALEUR DES INPUTS 
+
+/*
+const columnInputValue = document.querySelector('.formulaire__grille').value;
+
+console.log(columnInputValue);
+*/
